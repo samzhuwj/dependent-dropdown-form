ddb16f1236fb116748289ac77f670ae7cf8d5703
samzhuwj <samzhuwj@gmail.com>
2018-01-29 20:53:54 +0800

    dependent dropdown form

--- hr/forms.py
+++ hr/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from .models import Person, City
 
-
 class PersonForm(forms.ModelForm):
     class Meta:
         model = Person
@@ -10,3 +9,13 @@ class PersonForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['city'].queryset = City.objects.none()
+
+        data = kwargs.get('data')
+        if data is not None:
+            try:
+                country_id = int(data.get('country'))
+                self.fields['city'].queryset = City.objects.filter(country_id=country_id).order_by('name')
+            except ValueError:
+                pass  # invalid input from the client; ignore and fallback to empty City queryset
+        elif self.instance:
+            self.fields['city'].queryset = self.instance.country.city_set.order_by('name')
