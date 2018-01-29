cb3c53b82ebdeb97e37b54ba464ebb9a55852474
samzhuwj <samzhuwj@gmail.com>
2018-01-29 20:06:22 +0800

    form PersonForm, view load_cities

--- hr/views.py
+++ hr/views.py
@@ -1,6 +1,9 @@
+from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
-from .models import Person
+
+from .models import Person, City
+from .forms import PersonForm
 
 
 class PersonListView(ListView):
@@ -10,11 +13,17 @@ class PersonListView(ListView):
 
 class PersonCreateView(CreateView):
     model = Person
-    fields = ('name', 'birthdate', 'country', 'city')
+    form_class = PersonForm
     success_url = reverse_lazy('person_changelist')
 
 
 class PersonUpdateView(UpdateView):
     model = Person
-    fields = ('name', 'birthdate', 'country', 'city')
+    form_class = PersonForm
     success_url = reverse_lazy('person_changelist')
+
+
+def load_cities(request):
+    country_id = request.GET.get('country')
+    cities = City.objects.filter(country_id=country_id).order_by('name')
+    return render(request, 'hr/city_dropdown_list_options.html', {'cities': cities})
