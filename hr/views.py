5f705f5cd5ef4f79f2494abfc684b7faadf67fae
samzhuwj <samzhuwj@gmail.com>
2018-01-29 19:50:37 +0800

    view ListView, CreateView, UpdateView

--- hr/views.py
+++ hr/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from .models import Person
 
-# Create your views here.
+
+class PersonListView(ListView):
+    model = Person
+    context_object_name = 'people'
+
+
+class PersonCreateView(CreateView):
+    model = Person
+    fields = ('name', 'birthdate', 'country', 'city')
+    success_url = reverse_lazy('person_changelist')
+
+
+class PersonUpdateView(UpdateView):
+    model = Person
+    fields = ('name', 'birthdate', 'country', 'city')
+    success_url = reverse_lazy('person_changelist')
